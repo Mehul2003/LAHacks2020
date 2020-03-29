77de48d51ab662ecfeab8b46c7f9f326f243d5db
Mehul2003 <mehultailang@yahoo.com>
2020-03-29 01:34:29 -0700

    Initial Commit

---
+++ QuarenTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuarenTime
+//
+//  Created by Mehul Tailang on 3/29/20.
+//  Copyright © 2020 Mehul Tailang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
