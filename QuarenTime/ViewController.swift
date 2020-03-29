973f30ce24fafef464a03e8efa12bc3f87eff535
Mehul2003 <mehultailang@yahoo.com>
2020-03-29 01:52:02 -0700

    Updated UI

--- QuarenTime/ViewController.swift
+++ QuarenTime/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var selector: UISegmentedControl!
+    @IBOutlet var email: UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet var button: UIButton!
+    
+    var isLogin = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func handlePress(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            isLogin = true
+            button.setTitle("Login", for: .normal)
+        }
+        else {
+            isLogin = false
+            button.setTitle("Sign Up", for: .normal)
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
